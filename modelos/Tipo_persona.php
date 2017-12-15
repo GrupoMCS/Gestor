a14d77d93c35002144ad7112e90d2ce6af681b6a
Cometin111 <martin.corrales@grupomcs.com>
2017-12-14 18:57:54 -0700

    Se agrega modelo Tipo_persona

---
+++ modelos/Tipo_persona.php
@@ -0,0 +1,41 @@
+<?php
+    //Incluimos inicialmente la conexion a la base de datos
+    require "../config/Conexion.php";
+
+    Class Tipo_persona{
+        //Implementamos nuestro constructor
+        public function __construct(){
+
+        }
+
+        //Implementamos un metodo para insertar registros
+        public function insertar($detalle){
+            $sql="INSERT into tipo_persona (detalle, status)
+                    VALUES ('$detalle', 1)"; //status 1=activo 0=inactivo
+            return ejecutarConsulta($sql);
+        }
+
+        //Implementamos metodo para editar registros
+        public function editar($idtipo_persona, $detalle){
+            $sql="UPDATE tipo_persona SET detalle='$detalle'
+                    WHERE idtipo_persona='$idtipo_persona'";
+        }
+
+        //Implementamos un metodo para desactivar los tipos de persona
+        public function desactivar($idtipo_persona){
+            $sql="UPDATE tipo_persona SET status=0 
+                    WHERE idtipo_persona='$idtipo_persona'";
+        }
+
+        //Implementamos un metodo para activar los tipos de persona
+        public function activar($idtipo_persona){
+            $sql="UPDATE tipo_persona SET status=1 
+                    WHERE idtipo_persona='$idtipo_persona'";
+        }
+
+        //Implementar un metodo para mostrar los datos
+        public function mostrar($idtipo_persona){
+
+        }
+    }
+?>
